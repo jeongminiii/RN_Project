7c5971810f89d4a3ec6c5c644d473fe58239302f
JeongMin <jeongmono@naver.com>
2024-01-19 03:29:51 +0900

    first

--- App.js
+++ App.js
@@ -1,20 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import { Searchbar } from "react-native-paper";
+import { styles } from "./MainStyle";
+import Icon from "react-native-vector-icons/Ionicons";
 
 export default function App() {
+  const Menu = () => {};
+  const SearchScreen = () => {
+    const [searchQuery, setSearchQuery] = React.useState("");
+
+    const onChangeSearch = (query) => setSearchQuery(query);
+
+    return (
+      <Searchbar
+        placeholder="Search"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+        style={{ backgroundColor: "white" }}
+      />
+    );
+  };
+
+  const ButtonBox = () => {};
+
+  const Notice = () => {};
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.headerText}>청년 독립 만세</Text>
+        <TouchableOpacity onPress={Menu}>
+          <Text style={styles.MenuSt}>☰</Text>
+        </TouchableOpacity>
+      </View>
+      <SearchScreen />
+      <View style={{ marginTop: 10, marginBottom: 20 }}>
+        <Image
+          source={require("../240119/cdm.jpg")}
+          style={{ width: 350, height: 200 }}
+          resizeMode="cover"
+        />
+      </View>
+      <TouchableOpacity
+        onPress={Notice}
+        style={{ ...styles.Notice, flexDirection: "row" }}
+      >
+        <Icon name="notifications" size={20} color="#2e4b8f" />
+        <Text>알람창입니다</Text>
+      </TouchableOpacity>
+      <View style={styles.Row}>
+        <Text style={styles.TextBox}> @@님을 위한 맞춤 정책을 찾았어요</Text>
+        <TouchableOpacity onPress={ButtonBox} style={styles}>
+          <Text style={styles.In}>정책 더보기⮕ </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.btnST}>
+        <TouchableOpacity onPress={ButtonBox} style={styles.btnText}>
+          <Text>여기에는 박스가 들어갑니다</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.btnST}>
+        <TouchableOpacity onPress={ButtonBox} style={styles.btnText}>
+          <Text>여기에는 박스가 들어갑니다</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.btnST}>
+        <TouchableOpacity onPress={ButtonBox} style={styles.btnText}>
+          <Text>여기에는 박스가 들어갑니다</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.Row}>
+        <Text style={styles.TextBox}> 부동산 계약 관련 정보</Text>
+        <TouchableOpacity onPress={ButtonBox} style={styles}>
+          <Text style={styles.In}>정보 더보기⮕ </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={{ ...styles.btnST, flexDirection: "row" }}>
+        <TouchableOpacity onPress={ButtonBox} style={styles.btnText}>
+          <Text>박스</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={ButtonBox} style={styles.btnText}>
+          <Text>박스</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={ButtonBox} style={styles.btnText}>
+          <Text>박스</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ MainStyle.js
@@ -0,0 +1,61 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f2f3f8",
+    paddingHorizontal: 20,
+  },
+  header: {
+    //backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    flexDirection: "row",
+    marginTop: 50,
+    marginBottom: 10,
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    alignItems: "center",
+    marginRight: 90,
+    //backgroundColor: "orange",
+  },
+  MenuSt: {
+    fontSize: 35,
+    color: "black",
+  },
+  Notice: {
+    backgroundColor: "white",
+    padding: 10,
+    margin: 5,
+    borderRadius: 30,
+  },
+  Row: { flexDirection: "row", alignItems: "center" },
+  TextBox: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#2e4b8f",
+    padding: 5,
+
+    //backgroundColor: "orange",
+  },
+  In: { fontWeight: "bold" },
+  btnST: {
+    shadowColor: "#000000",
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+
+    margin: 3,
+  },
+  btnText: {
+    backgroundColor: "white",
+    padding: 10,
+    margin: 5,
+    borderRadius: 10,
+  },
+});
